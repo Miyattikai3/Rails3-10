3ba0d11fd7b2ec2c033ba8b32304e7de1ede9ac6
EC2 Default User <miyatchik@gmail.com>
2021-05-13 09:05:42 +0000

    [sample]3-7

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -2,20 +2,25 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
+  
   def create
     list = List.new(list_params)
     list.save
     redirect_to todolist_path(list.id)
   end
+  
   def index
     @lists = List.all
   end
+  
   def show
     @list = List.find(params[:id])
   end
+  
   def edit
     @list = List.find(params[:id])
   end
+  
   def update
     list = List.find(params[:id])
     list.update(list_params)
@@ -24,6 +29,6 @@ class TodolistsController < ApplicationController
   
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3812f4f04b10aebe7a3ef2b8514690cda1618d817d7be96d8898bb9a90388ad942f1e9d93a41da0485e4aa13cd7ff82e7a0b11c1377818d043e27d8b7f5a2c3b'
